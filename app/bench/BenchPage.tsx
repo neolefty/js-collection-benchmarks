491cc5fd3083fae8c217821af920276096e18f41
Beecher <bbbaker@alum.mit.edu>
2023-08-17 13:56:40 -0400

    move content up; hide more debug stuff

--- app/bench/BenchPage.tsx
+++ app/bench/BenchPage.tsx
@@ -12,20 +12,23 @@ export const BenchPage = () => {
             <form
                 onSubmit={setup.running ? setup.onCancel : setup.onStart}
                 action="#"
-                className="flex-grow grid content-center justify-items-center items-center gap-3 grid-cols-2"
+                className="grid justify-items-center items-center gap-3 grid-cols-2 pt-6"
             >
                 <BenchFormControls setup={setup} />
-                <div className="justify-self-end">Status</div>
-                <div className="justify-self-start">
-                    <strong>
-                        {[setup.start && "started", setup.running && "running"]
-                            .filter(Boolean)
-                            .join(", ") || "Not started"}
-                    </strong>
-                </div>
                 <BenchResultsDisplay setup={setup} />
                 <details className="col-span-2 w-full border-amber-700 py-2 border-y-2">
                     <summary className="text-center">State (debug)</summary>
+                    <p className="text-center py-3">
+                        Status:{" "}
+                        <strong>
+                            {[
+                                setup.start && "started",
+                                setup.running && "running",
+                            ]
+                                .filter(Boolean)
+                                .join(", ") || "Not started"}
+                        </strong>
+                    </p>
                     <code>
                         {JSON.stringify(setup)
                             .replaceAll(":", ": ")
